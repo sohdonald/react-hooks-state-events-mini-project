314da9fa74aeb20314a23ec7bc6f5ef90492bde5
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-05 12:01:36 -0400

    added crucial notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -9,39 +9,49 @@ console.log({ CATEGORIES, TASKS });
 
 function App() {
   // pass down the task data from App to TaskList
-  const [tasks,setTasks] = useState(TASKS);
+  const [tasks, setTasks] = useState(TASKS);
 
   // Pass the list of categories to this component from App
-  const [selectedCategory, setCategories] = useState("All")
+  const [selectedCategory, setCategories] = useState("All");
   // If the button for "All" is selected, all the tasks should be displayed.
 
   function handleTaskSubmit(task) {
-    setTasks((previousTask)=>[...previousTask, task])
+    setTasks((previousTask) => [...previousTask, task]);
   }
 
   function handleCategoriesSelect(category) {
-    setCategories(category)
+    setCategories(category);
   }
   function filterTasks() {
-    // The list of tasks being displayed should be filtered, 
+    // The list of tasks being displayed should be filtered,
     // so that only tasks that match the category that was clicked are displayed.
-    return selectedCategory === "All" 
-      ? tasks 
-      : tasks.filter(task => task.category === selectedCategory);
+    return selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
   }
   function handleDelete(text) {
- const updateTask = tasks.filter((task)=>task.text !== text) // give semantic meaning
-setTasks(updateTask);
-};
+    const updateTask = tasks.filter((task) => task.text !== text); // give semantic meaning
+    setTasks(updateTask);
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter onSelectCategory={handleCategoriesSelect}
-      selectedCategory={selectedCategory} categories={CATEGORIES}/>
-      <NewTaskForm onTaskFormSubmit={handleTaskSubmit} categories={CATEGORIES.filter(category => category !== "All")}/>
-      <TaskList tasks={filterTasks()}
-      
-      onDelete={handleDelete}/>
+      <CategoryFilter
+        onSelectCategory={handleCategoriesSelect}
+        // onSelectCategory is a prop from CategoryFilter, it is given the value of handleCategoriesSelect
+        // handleCategoriesSelect controls the STATE from the category array being displayed
+        selectedCategory={selectedCategory}
+        categories={CATEGORIES}
+        // selectedCategory is a prop from CategoryFilter, {selectedCategory} is the state being displayed
+        // what we see on the screen is the CURRENT STATE
+      />
+
+      <NewTaskForm
+        onTaskFormSubmit={handleTaskSubmit}
+        categories={CATEGORIES.filter((category) => category !== "All")}
+        // this makes select elements out of all the elements in CATEGORIES array except for "All"
+      />
+      <TaskList tasks={filterTasks()} onDelete={handleDelete} />
     </div>
   );
 }
